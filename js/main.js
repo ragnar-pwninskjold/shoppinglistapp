c6e7edd499c71f1530c235c711ddfa8f216bbf89
ragnar-pwninskjold <seankochel@gmail.com>
2016-06-25 19:52:49 -0400

    got delete button to show and hide

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
-// add item to "need" list
+
 
 $(document).ready(function(){
 
-	$(".inputbox").submit(function(event){
-		event.preventdefault();
-		var input = $("addition").val();
-		console.log("submitted");
-
+	// add item to "need" list
+	$("form").submit(function(event) {
+		event.preventDefault();
+		var input = $("#needed").val();
+		$(".need > ul").append("<li>"+input+"</li>");
 	});
+
+	// on hover, show the version with the "x" button
+
+	$("#lists").on("mouseenter", "li", function() {
+		var deleteButton = "<button class='deletebutton'>&#10006</button>"
+		$(this).append(deleteButton);
+	})
+	.on("mouseout", "li", function() {
+		$(".deletebutton").remove();
+	});
+
 });
 
-// on hover, show the version with the "x" button
 
 // if the "x" button is selected, remove the item entirely
 
