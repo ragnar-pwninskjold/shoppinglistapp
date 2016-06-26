b5dc20b0951a20926b88db920af0eca094adff23
ragnar-pwninskjold <seankochel@gmail.com>
2016-06-26 12:15:17 -0400

    fully functional first version

--- js/main.js
+++ js/main.js
@@ -2,26 +2,36 @@
 
 $(document).ready(function(){
 
-	// add item to "need" list
+	// add item to "need" list    &#10004
 	$("form").submit(function(event) {
 		event.preventDefault();
 		var input = $("#needed").val();
 		$(".need > ul").append("<li>"+input+"</li>");
+		$("#needed").val(" ");
 	});
 
 	// on hover, show the version with the "x" button
-
 	$("#lists").on("mouseenter", "li", function() {
-		var deleteButton = "<button class='deletebutton'>&#10006</button>"
+		var deleteButton = "<button id='deleteButton'>&#10006</button>"
 		$(this).append(deleteButton);
+		var checkMark = "<button id='checkMark'>&#10004</button>"
+		$(this).prepend(checkMark);
 	})
-	.on("mouseout", "li", function() {
-		$(".deletebutton").remove();
-	});
+	.on("mouseleave", "li", function() {
+	$("#deleteButton").remove();
+	$("#checkMark").remove();
+	})
+	//delete the item on click of the button
+	.on("click", "#deleteButton", function(){
+		$(this).closest("li").remove();
 
+	})
+	// if the item is clicked, transfer between list
+	.on("click", "#checkMark", function(){
+		$(this).closest("li").appendTo(".have").css("list-style", "none");
+	});
 });
 
 
-// if the "x" button is selected, remove the item entirely
 
-// if the item is clicked, transfer between list
+
